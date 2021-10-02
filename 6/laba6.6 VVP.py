b0db7d860f55427da77e84cb766b83aa1ca74b3b
FbiLove <lindafabianam@gmail.com>
2021-10-02 22:55:46 +0300

    6LABA

---
+++ 6/laba6.6 VVP.py
@@ -0,0 +1,5 @@
+print('Введите число. А = ')
+a = int(input())
+q = a**2
+ot = q * q * q * q
+print('A^8 = ', ot)
---
+++ 6/laba6.7 VVP.py
@@ -0,0 +1,6 @@
+print('Введите число. А = ')
+a = int(input())
+q = a**2
+p = a**5
+ot = q * q * q * q * q * p
+print('A^15 = ', ot)
