4a19f5823e0542e559fb32f692b8f1af8fb0c957
FabiLove <90962821+FabiLove@users.noreply.github.com>
2021-10-02 13:01:03 +0300

    Update laba5.3 VVP.py
    Условие С между А и В

--- 5/laba5.3 VVP.py
+++ 5/laba5.3 VVP.py
@@ -2,10 +2,13 @@ import math
 print('Найдем произведение длин отрезков AC и BC, имея их координаты на плоскости')
 print('Введите координаты первой, второй и третьей точек:')
 x1, y1, x2, y2, x3, y3 = int(input()), int(input()),int(input()),int(input()),int(input()),int(input())
-ac = (x2 - x1)**2 + (y2 - y1)**2
-bc = (x3 - x2)**2 + (y3 - y2)**2
-r1 = math.sqrt(ac)
-r2 = math.sqrt(bc)
-print('Отрезок AC = ', r1)
-print('Отрезок BC = ', r2)
-print('Произведение длин отрезков = ', r2 * r1)
+if not (x1 < x2 < x3 or y1 < y2 < y3):
+    print('Ошибка.')
+else:
+    ac = (x2 - x1)**2 + (y2 - y1)**2
+    bc = (x3 - x2)**2 + (y3 - y2)**2
+    r1 = math.sqrt(ac)
+    r2 = math.sqrt(bc)
+    print('Отрезок AC = ', r1)
+    print('Отрезок BC = ', r2)
+    print('Произведение длин отрезков = ', r2 * r1)
